32ccd4f60425ea8098febbed6fb0a9ff839ee8e2
Alisa Promthep <alisa.promthep@gmail.com>
2022-11-26 22:46:17 -0500

    updated text

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,7 +8,7 @@ const Hero = ({handleScrollToConnect}) => {
             <div className='hero__textbox'>
                 <h1 className='hero__headline'>
                     <span className='hero__headline--smaller'>I'm Alisa, a </span>Software Developer.</h1>
-                <p className='hero__tagline'>I love building digital products, with dreams and ambition to build tools, or applications for better lives. A side from technology, I am passionate in art, science and education. </p>
+                <p className='hero__tagline'> A former chemist, turned developer, building and designing to make lives easier!</p>
                 <button className='hero__button'
                 onClick={handleScrollToConnect}
                 >Send me a message</button>
