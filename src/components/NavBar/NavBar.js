15ac74b45789bd024bbb0fa37f2fbc54db88b46c
alisapromthep <alisa.promthep@gmail.com>
2024-07-30 14:46:35 -0700

    updated resume as pdf viwer

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,45 +1,66 @@
-import './NavBar.scss';
-import {Link} from 'react-router-dom';
-import {FaLinkedin, FaGithubSquare, FaTelegramPlane} from 'react-icons/fa';
+import "./NavBar.scss";
+import { Link } from "react-router-dom";
+import { FaLinkedin, FaGithubSquare, FaTelegramPlane } from "react-icons/fa";
 
-const NavBar = ({handleScrollToAbout, handleScrollToProject, openModal}) => {
-    return (
-        <div className='nav'>
-            <Link className='nav__name' to='/'>A.P.</Link>
-            <ul className='nav__list'>
-                <li className='nav__link-item'>
-                    <button className='nav__link nav__button'
-                    onClick={handleScrollToAbout}
-                    >About</button>
-                    </li>
-                <li className='nav__link-item'>
-                    <button className='nav__link nav__button'
-                    onClick={handleScrollToProject}>Projects</button>
-                    </li>
-                <li className='nav__link-item'>
-                    <a
-                className='nav__link'
-                href="https://resume.creddle.io/resume/rzivp3fp2e"
-                target="_blank"
-                rel="noreferrer">
-                    Resume
-                    </a>
-                </li>
-                <li className='nav__link-item'><button onClick={openModal}className='nav__button'><FaTelegramPlane className='nav__icon' size={'auto'}/></button></li>
-                <li className='nav__link-item'>
-                    <a href="https://www.linkedin.com/in/alisa-promthep/"
-                target="_blank"
-                rel="noreferrer"><FaLinkedin className='nav__icon' size={'auto'}/></a>
-                </li>
-                <li className='nav__link-item'><a 
-                href="https://github.com/alisapromthep"
-                target="_blank"
-                rel="noreferrer">
-                <FaGithubSquare size={'auto'} className='nav__icon'/>
-                </a></li>
-            </ul>
-        </div>
-    )
-}
+const NavBar = ({ handleScrollToAbout, handleScrollToProject, openModal }) => {
+  return (
+    <div className="nav">
+      <Link className="nav__name" to="/">
+        A.P.
+      </Link>
+      <ul className="nav__list">
+        <li className="nav__link-item">
+          <button
+            className="nav__link nav__button"
+            onClick={handleScrollToAbout}
+          >
+            About
+          </button>
+        </li>
+        <li className="nav__link-item">
+          <button
+            className="nav__link nav__button"
+            onClick={handleScrollToProject}
+          >
+            Projects
+          </button>
+        </li>
+        <li className="nav__link-item">
+          <a
+            className="nav__link"
+            href="/resume/alisapromthepresume.pdf"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Resume
+          </a>
+        </li>
+        <li className="nav__link-item">
+          <button onClick={openModal} className="nav__button">
+            <FaTelegramPlane className="nav__icon" size={"auto"} />
+          </button>
+        </li>
+        <li className="nav__link-item">
+          <a
+            href="https://www.linkedin.com/in/alisa-promthep/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin className="nav__icon" size={"auto"} />
+          </a>
+        </li>
+        <li className="nav__link-item">
+          <a
+            href="https://github.com/alisapromthep"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithubSquare size={"auto"} className="nav__icon" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
